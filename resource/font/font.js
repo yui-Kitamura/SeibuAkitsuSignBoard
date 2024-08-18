17fdc3be96dc67fef214e09b5821e3dc57015de8
yui-Kitamura <yui@yui-kitamura.eng.pro>
2024-08-19 01:29:00 +0900

    文字素材で自由なテロップを生成できるように

---
+++ resource/font/font.js
@@ -0,0 +1,73 @@
+/**
+ *
+ * @param origin 変換元テキスト
+ * @returns {string} spanタグで囲った変換後テキスト
+ */
+function getHtmlFromText(origin){
+    let res = '<span style="width:97%;height:72px;font-size:72px;' +
+        'position:absolute;display:inline-block;background-color:black;color:white" class="d16sqf">';
+    origin += "";
+    const inLen = origin.length;
+    for(let i=0; i<inLen; i++){
+        const cvt = findChar(origin.charAt(i));
+        res += '<span style="width:72px;height:72px;position:absolute;overflow:hidden;display:inline-block;' +
+            ' left:'+ i*72 +'px">'
+        if(cvt == null){
+            res += origin.charAt(i);
+        }else{
+            res +=  '<img src="./resource/font/charX8.png" class="led"' +
+                    ' style="top:'+cvt.getImgPos().h*-8+'px;left:'+cvt.getImgPos().w*-8 +'px;' +
+                    ' position:absolute;' +
+                    '" alt="'+origin.charAt(i)+'" />';
+        }
+        res += '</span>'
+    }
+    res += "</span>"
+    return res;
+}
+
+
+function charInfo() {
+    return {
+        c: "", row: 0, idx: 0,
+        getImgPos: function () {
+            return {h: 1 + this.row * 25, w: 1 + this.idx * 24}
+        }
+    }
+}
+charList = [];
+function findChar(c){
+    if(c == null || (c + "").length !== 1 ){
+        return null;
+    }
+    const listLen = charList.length;
+    for(let i=0; i<listLen; i++){
+        if(charList[i].c === c){
+            return charList[i];
+        }
+    }
+    return null;
+}
+function initCharFont(){
+    fetch("./resource/font/char.txt")
+        .then(response => response.text())
+        .then(text => {
+            const lines = text.split("\r\n")
+            let r = 0;
+            lines.forEach((line, rowIdx) => {
+                const lineLen = line.length;
+                for(let idx=0; idx < lineLen; idx++){
+                    if(line[idx] === '　'){ //全角スペース無視
+                        continue;
+                    }
+                    const myCharInfo = charInfo();
+                    myCharInfo.c = line[idx]; myCharInfo.row = r; myCharInfo.idx = idx;
+                    charList.push(myCharInfo);
+                }
+                r++;
+            });
+        })
+        .catch(error => {
+            console.error('char file fetch error', error);
+        });
+}
